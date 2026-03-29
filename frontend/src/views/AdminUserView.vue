efde15aff5dc4cc1c74cead102e5121e52760845
Guilherme Ramos Campestrini <gramoscampestrini@gmail.com>
2026-03-29 15:46:27 -0300

    feat: table for all users and their grades

--- frontend/src/views/AdminUserView.vue
+++ frontend/src/views/AdminUserView.vue
@@ -13,7 +13,28 @@ onMounted(async () => {
     <p>Você é um ADMIN</p>
     <h1>Em construção...</h1>
     <h1>Lista de usuários</h1>
-    <ul>
-        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
-    </ul>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Nota 1</th>
+                <th>Nota 2</th>
+                <th>Média</th>
+                <th>Faltas</th>
+                <th>Presença</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>{{ user.grade1 ?? "-" }}</td>
+                <td>{{ user.grade2 ?? "-" }}</td>
+                <td>{{ user.average ?? "-" }}</td>
+                <td>{{ user.absences ?? "-" }}</td>
+                <td>{{ user.attendance ?? "-" }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
